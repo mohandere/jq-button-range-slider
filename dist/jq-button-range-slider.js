ac05178381fbed7628d68fd93e4808c313ad3d0e
mohandere <deremohan01@gmail.com>
2017-02-21 12:53:41 +0530

    umd pattern added

--- dist/jq-button-range-slider.js
+++ dist/jq-button-range-slider.js
@@ -65,7 +65,7 @@
 
           _.instanceUid = instanceUid++;
 
-          _.render(true);
+          _.render();
 
       }
       return JqButtonRangeSlider;
@@ -104,7 +104,7 @@
     };
 
 
-    JqButtonRangeSlider.prototype.render = function(creation){
+    JqButtonRangeSlider.prototype.render = function(){
       var _ = this;
       //add default class
       _.$el.addClass(_.options.className);
@@ -125,13 +125,223 @@
         .resize(_.resizeHandler);
     };
 
-    JqButtonRangeSlider.prototype.slideHandler = function(e){
+    JqButtonRangeSlider.prototype.slideHandler = function(event){
 
+      var _ = this;
+      var $node = $(event.currentTarget);
+      var index = _.$sliderButtons.index($node);
+      var direction = _.getDirectionToMove(index);
+
+      switch (direction) {
+        case "L":
+          if (_.lastClickedButtonIndex === index ||
+            _.deActivateBtnAtIndex(index, direction)) {
+            index += 1;
+            _.setSliderBounds(index);
+
+          } else {
+            //if lower limit is not set then set both handle at same index
+            if (!_.isLowerBoundSet()) {
+              _.setSliderBounds(index);
+              _.setSliderBounds(index, 1);
+            } else {
+              //if current index is greater than upper limit and direction is L
+              //then set upper limit to index
+              if (index > _.getSliderBounds(1)) {
+                _.setSliderBounds(index, 1);
+              } else {
+                _.setSliderBounds(index);
+              }
+
+            }
+          }
+          break;
+        case "R":
+
+          if (_.lastClickedButtonIndex === index ||
+            _.deActivateBtnAtIndex(index, direction)) {
+            index -= 1;
+            _.setSliderBounds(index, 1);
+          } else {
+            //if upper limit is not set then set both handle at same index
+            if (!_.isUpperBoundSet()) {
+              _.setSliderBounds(index);
+              _.setSliderBounds(index, 1);
+            } else {
+              //if current index
+              if (index < _.getSliderBounds()) {
+                _.setSliderBounds(index);
+              } else {
+                _.setSliderBounds(index, 1);
+              }
+
+            }
+          }
+
+          break;
+      }
+      _.highlightsUI();
+      //store last clicked btn index
+      _.lastClickedButtonIndex = index;
+      //check wheather we have to reset slider slider
+      //if there is no any selected buttons then reset
+      var l = _.$el.find(".yo-range-btn.active").length;
+      if (!l) {
+        _.reset();
+      }
+      _.$el.trigger("yo:change", [_.getSliderValue(), _.getSliderRangeValue(), _.$el]);
+      return false;
+    };
+
+    JqButtonRangeSlider.prototype.getDirectionToMove = function(goal){
+      var _ = this;
+      var sliderLimits = [0, _.sliderMaxIndex];
+      //now after selection the slider middle will be move
+      if(_.isLowerBoundSet() && _.isUpperBoundSet() ){
+        sliderLimits = [_.getSliderBounds(), _.getSliderBounds(1)];
+      }
+      var closest = sliderLimits.reduce(function (prev, curr) {
+        return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
+      });
+      return (closest === sliderLimits[1]) ? "R" : "L";
+    };
+
+    JqButtonRangeSlider.prototype.deActivateBtnAtIndex = function(index, direction) {
+
+      var _ = this;
+      //if button is active and its first in active buttons list - when direction is L
+      //if button is active and its last in active buttons list - when direction is R
+      var l = _.$el.find(".yo-range-btn.active").length,
+        r, overallIndex;
+      if (!l) {
+        r = false;
+      }
+      switch (direction) {
+        case "L":
+          var $firstActiveBtn = _.$el.find(".yo-range-btn.active").eq(0);
+          overallIndex = _.$sliderButtons.index($firstActiveBtn);
+          r = overallIndex === index;
+          break;
+        case "R":
+          var $lastActiveBtn = _.$el.find(".yo-range-btn.active").eq(l - 1);
+          overallIndex = _.$sliderButtons.index($lastActiveBtn);
+          r = overallIndex === index;
+          break;
+      }
+      if (!r) {
+        r = false;
+      }
+      return r;
+    };
+
+    JqButtonRangeSlider.prototype.highlightsUI = function(){
+      var _ = this;
+      _.$sliderButtons.removeClass( "active" );
+      for (var i = _.sliderVars.lowerBound.index;
+        i <= _.sliderVars.upperBound.index; i++) {
+        _.$sliderButtons.eq(i).addClass( "active" );
+      }
+    };
+
+    JqButtonRangeSlider.prototype.isLowerBoundSet = function(){
+      var _ = this;
+      return _.sliderVars.lowerBound.index > -1;
+    };
+    JqButtonRangeSlider.prototype.isUpperBoundSet = function(){
+      var _ = this;
+      return _.sliderVars.upperBound.index > -1;
+    };
+
+    JqButtonRangeSlider.prototype.getSliderRangeValue = function(){
+      var _ = this;
+      var lv = _.$sliderButtons.eq(_.sliderVars.lowerBound.index).attr("value");
+      var uv = _.$sliderButtons.eq(_.sliderVars.upperBound.index).attr("value");
+      var r = {
+        "lb": {
+          index: _.sliderVars.lowerBound.index,
+          value: lv,
+        },
+        "ub": {
+          index: _.sliderVars.upperBound.index,
+          value: uv,
+        }
+      };
+      return r;
+    };
+
+    JqButtonRangeSlider.prototype.getSliderValue = function() {
+
+      var _ = this;
+      var r = [];
+      _.$el.find(".yo-range-btn.active").each(function(index, el) {
+        r.push($(el).val());
+      });
+      return r;
+    };
+
+    JqButtonRangeSlider.prototype.getSliderBounds = function(isUpper) {
+      var _ = this;
+      if (isUpper) {
+        return _.sliderVars.upperBound.index;
+      } else {
+        return _.sliderVars.lowerBound.index;
+      }
+    };
+
+    JqButtonRangeSlider.prototype.setSliderBounds = function(newIndex, isUpper) {
+      var _ = this;
+      if (isUpper) {
+        _.sliderVars.upperBound.index = newIndex;
+      } else {
+        _.sliderVars.lowerBound.index = newIndex;
+      }
+    };
+
+    //Plugins internal methods
+    //Exposed to users
+    JqButtonRangeSlider.prototype.reset = function() {
+      var _ = this;
+      _.setSliderBounds(-9999);
+      _.setSliderBounds(-9999, 1);
+      _.highlightsUI();
+      //store last clicked btn index
+      _.lastClickedButtonIndex = -9999;
+    };
+
+    //destroy current slider instance
+    JqButtonRangeSlider.prototype.destroy = function() {
+      var _ = this;
+      $.data(_.$el, 'jqButtonRangeSlider', null);
+      _.$el.unbind();
+      _.$el.html('');
+    };
+
+    //set new upper and lower bound
+    JqButtonRangeSlider.prototype.setRange = function(uiHash) {
+
+      var _ = this;
+      //check if lb and ub is valid or not
+      $.each(_.settings.sliderOptions, function(index, option) {
+        if (option.value === uiHash.lb) {
+          _.sliderVars.lowerBound.index = index;
+          _.sliderVars.lowerBound.value = uiHash.lb;
+        }
+        if (option.value === uiHash.ub) {
+          _.sliderVars.upperBound.index = index;
+          _.sliderVars.upperBound.value = uiHash.ub;
+        }
+      });
+      //set last clicked button as a upper one
+      _.lastClickedButtonIndex = _.sliderVars.upperBound.index;
+      //get indices of new lower and uper bound
+      _.highlightsUI();
     };
 
     JqButtonRangeSlider.prototype.resizeHandler = function(e){
 
     };
+
+
 
 
     $.fn.jqButtonRangeSlider = function() {
